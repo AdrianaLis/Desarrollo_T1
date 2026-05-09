5109821f60169fa3bfa06bdbd94eff9456e2e6ce
AdrianaLis <urbinalis2003@gmail.com>
2026-05-08 20:32:52 -0500

    Primer commit

--- algoritmo_voraz.py
+++ algoritmo_voraz.py
@@ -1,9 +1,10 @@
 def AlgoritmoVoraz(monedas, vuelto):
     for moneda in monedas:
-        cantidad = vuelto +0.00000001 // moneda
+        cantidad = (vuelto + 0.00000001) // moneda
         if cantidad > 0:
-            print(f"{cantidad} de S/{moneda} vuelto:{vuelto}")
             vuelto -= (moneda * cantidad)
+            print(f"{cantidad} de S/{moneda} vuelto:{round(vuelto, 2)}")
+           
 
 TiposMonedas = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05]
 
